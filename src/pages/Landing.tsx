4876e87e3d7b18069f51cc44c6054c3b0de2f443
igee-17 <igbagboleye@gmail.com>
2023-10-07 00:32:56 +0100

    first page in progress

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -6,12 +6,16 @@ const Landing = () => {
   return (
     <section>
         {/* NAVBAR */}
-        <nav>
-            <div><Logo aria-label="logo"/></div>
-            <div><Link to='/about'></Link><button>Sign in</button><button>Create an account</button></div>
+        <nav className='flex justify-between max-w-[1236px] m-auto p-3 bg-white' >
+            <div className='flex items-center'><Logo aria-label="logo"/></div>
+            <div className='flex gap-5 items-center'><Link to='/about' className='text-black text-center text-base font-medium leading-5'>About</Link>
+            <button className='text-black text-center text-base font-medium leading-5'>Sign in</button>
+            <button className='py-3 px-5 bg-[#DB3E4D] items-center rounded-md border-none text-white text-base leading-5'>Create an account</button>
+            </div>
         </nav>
+        
         {/* CONTENT */}
-        <div></div>
+        <div className='bg-[#3058A6] h-screen'></div>
     </section>
   )
 }
