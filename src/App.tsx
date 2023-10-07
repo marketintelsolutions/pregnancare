9ddb634edd7ae4ad0df9e64f3edb7cc87b9d227d
igee-17 <igbagboleye@gmail.com>
2023-10-07 11:54:18 +0100

    sign up page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
+import About from './pages/About';
 import Landing from './pages/Landing';
+import Signin from './pages/Signin';
 
 const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Landing/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/signin' element={<Signin/>}/>
         </Routes>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#3058A6',
+        'primary-red': '#DB3E4D',
+      },
+    },
   },
   variants: {},
   plugins: [],
